d2a8e1d338f73885d37db1e82d2780cecdbef7fc
dzaima <dzaimagit@gmail.com>
2021-12-30 02:08:25 +0200

    not worth its own file

--- diff.js
+++ diff.js
@@ -52,3 +52,19 @@ langs.diff = (...lang) => {
 }
 
 htmlgen.diff = (str, ...lang) => genDiff(str, lang);
+
+
+
+function genMixed(str) {
+  let val = str;
+  let res = "";
+  let [start, ...lns0] = str.split('\n');
+  return ("\n"+lns0.join('\n')).split('\n'+start).slice(1).map(c => {
+    let [argStr, ...lns1] = c.split('\n');
+    let [lang, ...args] = eval(argStr);
+    return htmlgen[lang](lns1.join('\n'), args);
+  }).join("<br>")
+}
+
+langs.mixed = () => genc.innerHTML = genMixed(main.value);
+htmlgen.mixed = (str) => genMixed(str);
